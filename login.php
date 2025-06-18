6ee7a17191743b2a355a62a44beaf85776fa8375
Nicolas Selig <triptonn.dev@gmail.com>
2025-06-18 11:26:59 +0200

    Bare bones Login page

---
+++ login.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <link rel="stylesteet" href="style.css" />
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Classic Models - Mitarbeiter-Login</title>
+</head>
+<body>
+
+</body>
