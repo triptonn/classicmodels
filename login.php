ef43336f49d215e3656d613b2ba6041309ad6bde
Nicolas Selig <triptonn.dev@gmail.com>
2025-06-18 15:03:27 +0200

    Automatische DB anpassung
    Adding "hash" column to employees table and adding a value for a test
    employee

--- login.php
+++ login.php
@@ -33,24 +33,109 @@
 <?php
 ///////////////////////////////////////////////////////////////////////////////
 // Database modifications to make exercise work:
+// - adding column "hash" to the employee table of 'classicmodels'
 ///////////////////////////////////////////////////////////////////////////////
 
 
+$host = "localhost";
+$db = "classicmodels";
+$admin = "root";
+$admin_pwd = "";
+$charset = "utf8mb4";
+
+$dsn = "mysql:host=$host;dbname=$db;";
+
+try {
+    $conn = new PDO($dsn, $admin, $admin_pwd);
+    $conn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $sql = <<<HERE
+    SELECT COUNT(*) as col_count
+    FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE TABLE_SCHEMA = :db
+    AND TABLE_NAME = 'employees'
+    AND COLUMN_NAME = 'hash'
+    HERE;
+
+    $stmt = $conn->prepare($sql);
+    $stmt->execute(['db' => $db]);
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if(!($result['col_count'] > 0)) {
+        new_line();
+        new_line();
+        echo "DB needs to be modified...";
+        new_line();
+
+        try {
+        $create_column_sql = <<<HERE
+        ALTER TABLE `employees` ADD `hash` VARCHAR(60) NOT NULL AFTER `jobTitle`;
+        HERE;
+
+        $stmt = $conn->prepare($create_column_sql);
+        $stmt->execute();
+        } catch (Exception $e) {
+            echo "Exception caught: ".$e->getMessage();
+            new_line();
+        }
+
+        echo "Created 'hash' column in table employees.";
+        new_line();
+
+        $add_test_pwd_sql = <<<HERE
+        UPDATE `employees`
+        SET `hash` = '$2y$10$/ulWpIi3aWR3Xi6qghXZkueK3twjLYUnlTbiToUaI.wyOeX9WPdr6'
+        WHERE `employeeNumber` = 1102;
+        HERE;
+
+        $stmt = $conn->prepare($add_test_pwd_sql);
+        $stmt->execute();
+
+        echo "Fake Password: '1111' added for employee with email: gbondur@classicmodelcars.com";
+        new_line();
+    }
 
 
+} catch (Exception $e) {
+    echo "Exception caught: ".$e->getMessage();
+}
 
 ///////////////////////////////////////////////////////////////////////////////
 ?>
 
 <?php
+
 if(!empty($_POST)) {
     $username = htmlspecialchars(trim($_POST["username"]));
     $pwd_hash = password_hash(htmlspecialchars(trim($_POST["password"])), PASSWORD_BCRYPT);
 
-    echo "Username: ".$username;
+    /* echo "Username: ".$username;
     new_line();
-    echo "Password hash: ".$pwd_hash;
+    echo "Password hash: ".$pwd_hash; */
 }
+
+// hash for pwd '1111': 
+
+
+$host = "localhost";
+$db = "classicmodels";
+$charset = "utf8mb4";
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE               => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE    => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES      => false,
+];
+
+session_start();
+
+try {
+    $conn = new PDO($dsn, $username, $pwd_hash, $options);
+} catch (Exception $e) {
+    echo "Exception caught: ".$e->getMessage();
+}
+
 ?>
 
 
