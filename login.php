d6af13a49118197f1e3a4f2c5cb9ee118088fac8
Nicolas Selig <triptonn.dev@gmail.com>
2025-06-18 13:19:50 +0200

    little login body

--- login.php
+++ login.php
@@ -1,11 +1,32 @@
 <!DOCTYPE html>
-<html lang="de">
+<html lang="en">
 <head>
-    <link rel="stylesteet" href="style.css" />
+    <link rel="stylesheet" href="style.css" />
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Classic Models - Mitarbeiter-Login</title>
 </head>
 <body>
+
+
+<h1 class="page_title">Mitarbeiter Login</h1>
+
+<form method="post" action="">
+    <table>
+        <td>
+        <h2 class="input_name">Email: </h2>
+        </td>
+        <td>
+        <input type="text" placeholder="Benutzernamen eingeben" name="username" required>
+        </td>
+        <td>
+        <h2 class="input_name">Passwort: </h2>
+        </td>
+        <td>
+        <input type="password" placeholder="Passwort eingeben" name="password" required>
+        </td>
+    </table>
+    <input type="submit" value="Absenden">
+</form>
 
 </body>
